b7c845fe7ea4f2ae03647638bcf21817cf8b4031
Mizore <xvz@live.com>
2021-01-25 14:09:57 +0800

    Update ga_all.ts

--- api/ga_all.ts
+++ api/ga_all.ts
@@ -1,1 +1,85 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import { google } from 'googleapis'
+import config from './_config'
 
+/**
+ * Blog hit count. Served by Google Analytics
+ */
+export default async (req: NowRequest, resp: NowResponse) => {
+  // API query page parameter
+  const { page = '' } = req.query
+
+  const hostname = config.hostname;
+
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      private_key: config.auth.privateKey,
+      client_email: config.auth.clientEmail,
+    },
+    projectId: config.auth.projectId,
+    scopes: 'https://www.googleapis.com/auth/analytics.readonly',
+  })
+  const client = await auth.getClient()
+  const analyticsreporting = google.analyticsreporting({
+    version: 'v4',
+    auth: client,
+  })
+
+  const gaReport = await analyticsreporting.reports.batchGet({
+    requestBody: {
+      reportRequests: [
+        {
+          viewId: config.viewId,
+          dateRanges: [
+            {
+              startDate: config.startDate,
+              endDate: 'today',
+            },
+          ],
+          metrics: [
+            {
+              expression: 'ga:pageviews',
+            },
+          ],
+          dimensions: [
+            {
+              name: 'ga:hostname',
+            },
+          ],
+          dimensionFilterClauses: [{
+            filters: [
+              {
+                'dimensionName': 'ga:hostname',
+                'operator': 'EXACT',
+                'expressions': [hostname] as string[],
+              }
+            ],
+          }],
+          orderBys: [
+            {
+              fieldName: 'ga:pageviews',
+              sortOrder: 'DESCENDING',
+            },
+          ],
+        },
+      ],
+    },
+  })
+  const report = gaReport.data.reports[0].data
+
+  let res = []
+  if (report.totals[0].values[0] === '0') {
+    res = [{ page: page, hit: '0' }]
+  } else {
+    report.rows.forEach(r => {
+      // Remove all pages with querys
+      if (!r.dimensions[0].includes('?')) {
+        res.push({ page: r.dimensions[0], hit: r.metrics[0].values[0] })
+      }
+    })
+  }
+
+  resp.setHeader('Access-Control-Allow-Origin', '*')
+  resp.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+  resp.status(200).send(res)
+}
