24cded29a2fbe8dd8cd5b37fb71bdad7a049f606
Mizore <xvz@live.com>
2021-01-26 10:06:11 +0800

    Update ga.ts

--- api/ga.ts
+++ api/ga.ts
@@ -95,6 +95,6 @@ export default async (req: NowRequest, resp: NowResponse) => {
   }
 
   resp.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Cache-Control', 'public, max-age=60, s-maxage=60, stale-while-revalidate')
+  resp.setHeader('Cache-Control', 'public, max-age=60, s-maxage=60, stale-while-revalidate')
   resp.status(200).send(res)
 }
